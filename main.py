77c3cccdb16d7471674fa17077a83f9dce52719c
HARSHGURU <126169204+HARSHGURU@users.noreply.github.com>
2023-06-02 23:22:29 +0530

    main.py

--- main.py
+++ main.py
@@ -2,14 +2,14 @@ PLACEHOLDER = "[name]"
 
 
 
-with open("C:/Users/HP/OneDrive/Desktop/100 days/100 days codes/day-24/Mail+Merge+Project+Start/Mail Merge Project Start/Input/Names/invited_names.txt") as names_file:
+with open("Give Path of the file were names of the preson is saved i.e invited_names") as names_file:
     names = names_file.readlines()
 
 
-with open("./Input/Letters/starting_letter.txt") as letter_file:
+with open("Give path of the file were body of the letter is saved i.e starting_letter") as letter_file:
     letter_contents = letter_file.read()
     for name in names:
         stripped_name = name.strip()
         new_letter = letter_contents.replace(PLACEHOLDER, stripped_name)
-        with open(f"./Output/ReadyToSend/letter_for_{stripped_name}.txt", mode="w") as completed_letter:
+        with open(f"./Output/ReadyToSend/letter_for_{stripped_name}.txt", mode="w") as completed_letter: #here give path of the folder were you have to save generated message.
             completed_letter.write(new_letter)
